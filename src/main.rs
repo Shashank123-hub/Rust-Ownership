7ba4feb09008f8f83fe90eb62d15c2eaac2fbb46
Shashank <shashankthakur462@gmail.com>
2025-04-13 14:49:41 +0530

    updated the src code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+const VAR_C: i8 = 50; //A global variable - this is available throughout the program
+
 fn scope_of_var(a: i8) {
     {
         let var_b:i8 = 5;  // scope of this variable is available within the braces
         println!("Variable b is: {}", var_b);  //This will works as var_b is in scope
+        println!("Variable c is: {}", VAR_C);  //Variable c is a global variable and is available here
     }
     println!("Variable a is: {}", a);  //This will work
     //println!("Variable b is: {}", var_b);  //This will give an error as scope of var_b is not available here;
